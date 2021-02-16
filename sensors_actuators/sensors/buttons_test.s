3f4315baf9592e77658b8da9a3f16a1d79568eaa
Olivier Joris <45203619+OlivierJoris@users.noreply.github.com>
2021-02-16 19:19:14 +0100

    update button_tests

--- sensors_actuators/sensors/buttons_test.s
+++ sensors_actuators/sensors/buttons_test.s
@@ -11,31 +11,64 @@
     PSECT text, abs, class=CODE, delta=2
 
     org 	00h
-	goto    start
+	 goto start
 
 start:
-    movlb 01h			;Go to bank 1
+    call initialisation
+    goto main_loop
+
+initialisation:
+    movlb 01h			   ;Go to bank 1
     movlw 00001111B		;Set the port pin types of the RE
     movwf TRISE			;RE0, RE1, RE2, and RE3 are inputs
 
     movlw 01111111B     ;Set the port pin types of the RD
     movwf TRISD			;RD7 is output
-    movlb 00h			;Go to bank 0
+    
+    movlb 02h			    
+    clrf LATD			   ;Clear output of all PORTD
+    
+    movlb 03h        
+    movlw 00000000B         
+    movwf ANSELE        ;Set all PORTE as digital 
+    movlw 00000000B         
+    movwf ANSELD         ;Set all PORTD as digital 
+    
+    ; configure the clock
+    movlb 01h
+    movlw 01101110B
+    movwf OSCCON
+    movlw 00000000B
+    movwf OSCTUNE
+    
+    return
 
 main_loop:
-    btfss PORTE, 0	    ;Read RE0 pin
+    movlb 00h
+    btfss PORTE, 0	   ;Read RE0 pin
     goto button_is_off	;If RE0 is '0', then go to button_is_off
+    goto button_is_on   ;Else go to button_is_on
+    movlb 00h
     btfss PORTE, 1      ;Read RE1 pin
     goto button_is_off  ;If RE1 is '0', then go to button_is_off
+    goto button_is_on   ;Else go to button_is_on
+    movlb 00h
     btfss PORTE, 2      ;Read RE2 pin
     goto button_is_off  ;If RE2 is '0', then go to button_is_off
+    goto button_is_on   ;Else go to button_is_on
+    movlb 00h
     btfss PORTE, 3      ;Read RE3 pin
     goto button_is_off  ;If RE3 is '0', then go to button_is_off
+    goto button_is_on   ;Else go to button_is_ons
 
 button_is_on:
-    bsf PORTD, 7		;Set RD7 output
-    goto main_loop		;Go back to the main loop
+    movlb 02h
+    movlw 10000000B	   ;Set RD7 output
+    movwf LATD
+    goto main_loop	   ;Go back to the main loop
 
 button_is_off:
-    bcf PORTD, 7		;Clear RD7 output
-    goto main_loop		;Go back to the main loop
+    movlb 02h
+    movlw 00000000B	   ;Clear RD7 output
+    movwf LATD
+    goto main_loop	   ;Go back to the main loop
