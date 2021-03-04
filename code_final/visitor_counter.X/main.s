99216f0564013823f7191fe72bc6d88ea3f50585
Maxime Goffart <maximegoffart@icloud.com>
2021-03-04 10:24:27 +0100

    handle_setup

--- code_final/visitor_counter.X/main.s
+++ code_final/visitor_counter.X/main.s
@@ -16,8 +16,7 @@
     org 04h
     goto interrupt_routine
     
-array:
-    movf /*need to be filled */, 0 ;w <- counter (p.117 in datasheet)
+array:		    ;Need to put value in w before calling
     addwf PCL, 1    ;w is added to PCL in order to jump to the corresponding line
                     ;which is returning a literal value into w (retlw) and
                     ;comes back to the originated call.
@@ -116,6 +115,7 @@ initialisation:
     tenDigit		EQU 24h	    ;Current ten digit
     unitDigit		EQU 25h	    ;Current unit digit
     needUpdateDisplay	EQU 26h	    ;If display need to be updated
+    maxDigit		EQU 27h	    ;Max value for a digit
  
     ;Set variables
     clrf mode
@@ -125,6 +125,8 @@ initialisation:
     clrf tenDigit
     clrf unitDigit
     clrf needUpdateDisplay
+    movlw 00001010B	;10 in decimal
+    movwf maxDigit
     
     ;Start timer 1
     movlb 00h
@@ -139,9 +141,101 @@ main_loop:
     goto handle_operation
 
 handle_setup:
-    ;Need to be filled
+    ;Handles the buttons B1->B4 during the selection of the
+    ;maximum number of customers
+    
+    movlb 00h
+    btfsc PORTE, 0	;Check minus button
+    call digit_minus
+    btfsc PORTE, 1	;Check plus button
+    call digit_plus
+    btfsc PORTE, 2	;Check next digit button
+    call next_digit
+    ;Reset button is not considered because we are
+    ;are already in the setup mode
+    call update_setup_display
     
     goto main_loop
+    
+digit_minus:
+    ;Decrements limitTenDigit or limitUnitDigit based on
+    ;selectedDisplay and current respective value
+
+    movlb 00h
+    btfss selectedDisplay, 0	;Check which variable need to be modified
+    goto digit_minus_ten
+    goto digit_minus_unit
+    
+digit_minus_ten:		;Update variable for tens
+    movlw 00000000B
+    subwf limitTenDigit, 0
+    btfss STATUS, 0		;Check if limitTenDigit == 0
+    decf limitTenDigit, 1	;If not, decrement it
+    goto digit_minus_end
+    
+digit_minus_unit:		;Update variable for units
+    movlw 00000000B
+    subwf limitUnitDigit, 0
+    btfss STATUS, 0		;Check if limitUnitDigit == 0
+    decf limitUnitDigit, 1	;If not, decrement it
+    goto digit_minus_end
+
+digit_minus_end:
+    return
+    
+digit_plus:
+    ;Increments limitTenDigit or limitUnitDigit based on
+    ;selectedDisplay and current respective value
+    
+    movlb 00h
+    btfss slected_display, 0	;Check which variable need to be modified
+    goto digit_plus_ten
+    goto digit_plus_unit
+    
+digit_plus_ten:			;Update variable for tens
+    movf limitTenDigit
+    subwf maxDigit, 0
+    btfss STATUS, 0		;Check if limitTenDigit == maxDigit
+    incf limitTenDigit, 1	;If not, increment it
+    goto digit_plus_end
+digit_plus_unit:		;Update variable for units
+    movf limitUnitDigit
+    subwf maxDigit, 0
+    btfss STATUS, 0		;Check if limitUnitDigit == maxDigit
+    incf limitUnitDigit, 1	;If not, increment it
+    goto digit_plus_end
+    
+digit_plus_end:
+    return
+    
+next_digit:
+    ;Modifies which of the 2 7-segments is going
+    ;to be modified
+
+    movlb 00h
+    btfsc selectedDisplay, 0
+    goto next_digit_left
+    goto next_digit_right
+next_digit_left:
+    clrf selectedDisplay
+    goto next_digit_end
+next_digit_right:
+    movlw 00000001B
+    movwf selectedDisplay
+next_digit_end:
+    return
+    
+update_setup_display:
+    ;Updates the 7-segments during the setup phase
+
+    movlb 00h
+    movf limitTenDigit, 0
+    call array
+    movwf PORTA
+    movf limitUnitDigit, 0
+    call array
+    movwf PORTB
+    return
     
 handle_operation:
     ;Need to be filled
